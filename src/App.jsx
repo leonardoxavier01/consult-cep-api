c3c5a48f13b75478e64c28d6312e1e8fdff090c3
unknown <leonardoxavier092@gmail.com>
2022-02-22 23:30:24 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="Title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu cep"/>
+
+        <button className="buttonSearch">
+          Procurar
+        </button>
+      </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
