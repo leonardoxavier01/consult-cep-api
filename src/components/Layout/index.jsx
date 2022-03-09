46ef032a1f06c2fa006d53d648f7309c9f304acd
unknown <leonardoxavier092@gmail.com>
2022-03-09 00:32:51 -0300

    style: add icon RiRoadMapLine

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import InputCep from "../InputCep";
 import { ContainerCep, Title} from "./styles";
+import {RiRoadMapLine} from 'react-icons/ri'
 
 const Layout = () => (
   <ContainerCep>
-    <Title>Consulte seu Cep</Title>
+    <Title>Consulte seu Cep <RiRoadMapLine/></Title>
     <InputCep />
   </ContainerCep>
 );
